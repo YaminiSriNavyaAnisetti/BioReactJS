634634ce9b4cc75b7c3afe1dc136fb06e028233b
YaminiSriNavyaAnisetti <137883714+YaminiSriNavyaAnisetti@users.noreply.github.com>
2023-07-28 20:49:17 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,54 @@
+ReactDOM.render(
+    <div>
+    <h1 >Biography of Nick Vujicic</h1>
+    <img src="https://images.yourstory.com/cs/1/4f6e44208dcd11ea9b5adddb657cbcf8/Nick-Vujicic-1589010761832.jpeg"></img>
+    <p>
+    Nicholas James Vujicic was born on 4 December 1982.He is an Australian American Christian evangelist and motivational speaker of Serbian descent. Vujicic has tetra-amelia syndrome, a disorder characterised by the absence of arms and legs.
+    </p>
+<h3>Early life</h3>
+Vujicic was born in Melbourne, Australia, in 1982 to Dušanka and Borislav Vujičić, Serbian immigrants from Yugoslavia. Vujicic's father is a pastor. Vujicic was born with tetra-amelia syndrome, a rare disorder characterised by the absence of arms and legs.
+
+According to Vujicic's autobiography, his mother refused to see him or hold him immediately after his birth; instead, she and her husband left the hospital. He was bullied at school because of his medical condition. At one point, Vujicic attempted to commit suicide by drowning himself, but he was saved. While Vujicic's parents initially struggled with grief and confusion about his condition, they "raised him to look at the bright side of life".
+
+Originally, the toes of one of Vujicic's feet were fused. An operation was performed to separate the toes so that he can use them as fingers. Vujicic refers to this foot as his "chicken drumstick".
+
+When Vujicic was 17 years old, he started giving inspirational talks at school and church-sponsored events.
+
+Vujicic graduated from Griffith University at the age of 21 with a Bachelor of Commerce degree, with a double major in accountancy and financial planning.[better source needed]
+
+<h3>Ministry</h3>
+Vujicic is a Christian and an evangelist. He travels and speaks about his testimony of faith in Jesus Christ. His ministry is known as Life Without Limbs. As of 2008, the ministry was based in southern California.
+
+Vujicic appeared on the ABC television show 20/20 in 2008.
+
+Vujicic starred in the short film The Butterfly Circus (2009). At the 2010 Method Fest Independent Film Festival, he was awarded Best Actor in a Short Film for his starring performance.
+
+Vujicic's first book, Life Without Limits: Inspiration for a Ridiculously Good Life, was published by Random House in 2010 and has been translated into 30 languages.
+
+Vujicic is opposed to abortion.In 2021, he co-founded ProLife Bank.
+
+In 2022, Vujicic launched a new ministry initiative called "Champions for the Brokenhearted". The goal of the project is to "support particular disaffected groups in need".
+
+<h3>Personal life</h3>
+
+Vujicic speaking to students in Florida in 2015
+Vujicic married Kanae Miyahara on February 12, 2012.As of 2017, Vujicic and his wife have two sons and two daughters.The family resides in Southern California.
+
+Without any limbs, Vujicic is able to type 43 words per minute on a computer.
+
+<h3>Books and publications</h3>
+<ul>
+<li>Life Without Limits: Inspiration for a Ridiculously Good Life (2010)</li>
+<li>Your Life Without Limits (2012)</li>
+<li>Limitless: Devotions for a Ridiculously Good Life (2013)</li>
+<li>Unstoppable: The Incredible Power of Faith in Action (2013)</li>
+<li>The Power of Unstoppable Faith (2014)</li>
+<li>Stand Strong (2015)</li>
+<li>Love Without Limits (2016)</li>
+</ul>
+
+
+    </div>,
+    document.getElementById("content")
+
+)
